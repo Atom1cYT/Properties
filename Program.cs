88057b0dbb5c0fda826b347ae585d0bb9f2008a3
Atom1cYT <146125539+Atom1cYT@users.noreply.github.com>
2024-04-08 08:51:10 -0500

    Properties Bruner Zach

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Properties;
+using System;
+
+namespace Properties_Bruner_Zach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enters Ford Mustang into the system
+            car car = new car();
+            car.Make = "Ford";
+            car.Model = "Mustang";
+            // Makes the car run into the comman prompt
+            Console.WriteLine($"Make {car.Make}, Model: {car.Model}");
+            // Makes a New Chevy Camero into the command prompt
+            car car2 = new car();
+            car2.Make = "Chevy";
+            car2.Model = "Camero";
+            Console.WriteLine($"Make {car2.Make}, Model: {car2.Model}");
+            // Enters MAke and model into the system for car 3
+            car car3 = new car();
+            Console.WriteLine($"Make {car3.Make}, Model: {car3.Model}");
+        }
+    }
+}
---
+++ car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class car
+    {
+
+        private string model;
+
+        public string Model { get { return model; } set { model = value; } }
+        // gets the return Model and set model value
+        public string Make {  get; set; }
+        // Getsd the make set and get
+
+
+    }
+}
